6c9a674216c5e30582e79e9baea1a1250d99a6f8
BakareHalimat <halimatbakare10@gmail.com>
2022-04-21 00:56:28 +0100

    week4assignment

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace week4assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("This is a simple app to check prime number designed by Fuhad");
+          Console.Write("Input the number to be checked...");
+          int number = int.Parse(Console.ReadLine());
+
+          if (number == 17)
+          {
+              Console.WriteLine( "Prime number");
+          }
+          else if (number == 57)
+          {
+              Console.WriteLine("Not prime number");
+          }
+          
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
